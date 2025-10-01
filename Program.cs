c4c113136a17265778374650bb91f5f406c73a97
Ana Cecília <122322406+ana-ceci@users.noreply.github.com>
2025-10-01 20:19:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+using System.Net.Http;
+using ConsumerViaCep.Models;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        WriteLine("Digite seu CEP:");
+        var cep = ReadLine();
+        var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+        using var client = new HttpClient();
+
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(enderecoUrl);
+
+            response.EnsureSuccessStatusCode();
+            string respostaApiJson = await response.Content.ReadAsStringAsync();
+            Endereco? endereco = System.Text.Json.JsonSerializer.Deserialize<Endereco>(respostaApiJson);
+
+
+
+            WriteLine("CEP:\n" + endereco.cep);
+            WriteLine("LOGRADOURO:\n" + endereco.logradouro);
+            WriteLine("BAIRRO:\n" + endereco.bairro);
+            WriteLine("CIDADE:\n" + endereco.localidade);
+        }
+        catch (System.Exception e)
+        {
+            WriteLine("Erro ao acessar a API: " + e.Message);
+        }
+    }
+}
